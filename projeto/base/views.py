1cbc2d6f8eab06ba5f23afabfe333ef64b4e0686
Luma Rodrigues <rdsluma@gmail.com>
2020-09-18 18:40:42 -0300

    Primeira tentativa de criação do formulário de contato

--- projeto/base/views.py
+++ projeto/base/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from projeto.forms import ContactForm
+from django.core.mail import send_mail
 
 
 def home(request):
@@ -14,4 +16,13 @@ def sketchbook(request):
 
 
 def contact_me(request):
-    return render(request, 'base/contact_me.html')
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            send_mail('', message, email, ['rdsluma@gmail.com', email])
+    return render(request, 'base/contact_me.html', {'form': form})
--- projeto/settings.py
+++ projeto/settings.py
@@ -185,4 +185,10 @@ if AWS_ACCESS_KEY_ID:
 
 
 
-
+# gmail settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rdsluma@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
