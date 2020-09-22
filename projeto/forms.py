0a6725235b0af636b88faf60f71a627195a9d029
Luma Rodrigues <rdsluma@gmail.com>
2020-09-22 17:05:38 -0300

    Criado formulário de contato e atualização de dependências close #88

--- projeto/forms.py
+++ projeto/forms.py
@@ -4,4 +4,5 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, required=False)
     email = forms.EmailField(max_length=100, required=True)
+    subject = forms.CharField(max_length=100, required=False)
     message = forms.CharField(widget=forms.Textarea)
